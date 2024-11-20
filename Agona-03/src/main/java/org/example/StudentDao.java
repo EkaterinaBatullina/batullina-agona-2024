423a0196dc35b83987b9eaf5acc544a13efe5f9f
Ekaterina Batullina <batullina2005@gmail.com>
2024-11-20 17:38:14 +0000

    Update StudentDao.java

--- Agona-03/src/main/java/org/example/StudentDao.java
+++ Agona-03/src/main/java/org/example/StudentDao.java
@@ -83,7 +83,7 @@ public class StudentDao extends AbstractDao<Student> {
         }
     }
 
-    boolean save(Student entity) {
+    void save(Student entity) {
         try {
             PreparedStatement statement = getConnection().map(connection -> {
                 try {
@@ -99,7 +99,9 @@ public class StudentDao extends AbstractDao<Student> {
                 }
             }).orElseThrow(StudentException::new);
             int rowsAffected = statement.executeUpdate();
-            return rowsAffected > 0;
+            if (rowsAffected <= 0) {
+                throw new StudentException("Save failed");
+            }
         } catch (SQLException e) {
             throw new StudentException("Error executing save query");
         }
