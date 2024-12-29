dfb3fad4feaa987c1be40b72b81f8d27cbe5a11a
EkaterinaBatullina <batullina2005@gmail.com>
2024-12-29 16:17:43 +0300

    Agona-09

--- Agona-09/spring-impl/src/main/java/org/example/repository/AuthorProfileRepository.java
+++ Agona-09/spring-impl/src/main/java/org/example/repository/AuthorProfileRepository.java
@@ -4,9 +4,11 @@ import org.example.model.AuthorProfileEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface AuthorProfileRepository extends JpaRepository<AuthorProfileEntity, UUID> {
 
     Page<AuthorProfileEntity> findAll(Pageable pageable);
--- Agona-09/spring-impl/src/main/java/org/example/repository/AuthorRepository.java
+++ Agona-09/spring-impl/src/main/java/org/example/repository/AuthorRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<AuthorEntity, UUID> {
 
     @Query("SELECT author FROM AuthorEntity author LEFT JOIN FETCH author.books LEFT JOIN FETCH author.profile")
